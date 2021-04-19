deba97ef603fc8c4ed0b8d7f90b86e1167c756b7
Maxim <bolshovm@yandex.ru>
2021-04-19 18:02:07 +0300

    del hw4-old

--- spring-boot-hw4/src/main/java/com/example/springboothw4/configuration/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.springboothw4.configuration;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("com.example.springboothw4")
-public class AppConfig {
-}
