44b730bc68e3d3a013b34a47d577450fa4771167
Maxim <bolshovm@yandex.ru>
2021-05-03 12:57:15 +0300

    change lesson6 HW

--- spring-boot-hw4/src/main/java/com/example/springboothw4/lesson6/MainApp.java
+++ spring-boot-hw4/src/main/java/com/example/springboothw4/lesson6/MainApp.java
@@ -1,66 +1,14 @@
 package com.example.springboothw4.lesson6;
 
-import com.example.springboothw4.lesson6.entities.Costumer;
-import com.example.springboothw4.lesson6.entities.Product;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.stream.Collectors;
-
 public class MainApp {
-
 
     public static void main(String[] args) {
 
-        prepareData();
-        getProductByIdCostumer(3);
+        Factory factory = new Factory();
+        factory.prepareData();
+        factory.getProductByIdCostumer(2L);
+        factory.getCostumerByIdProduct(2L);
 
-    }
-
-    private static void prepareData() {
-        SessionFactory factory = new Configuration()
-                .configure(("hibernate.cfg.xml"))
-                .buildSessionFactory();
-
-
-        Session session = null;
-
-        try {
-            String sql = Files.lines(Paths.get("src/main/java/com/example/springboothw4/lesson6/createDB.sql")).collect(Collectors.joining(" "));
-            session = factory.getCurrentSession();
-            session.beginTransaction();
-            session.createNativeQuery(sql).executeUpdate();
-            session.getTransaction().commit();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            factory.close();
-            if (session != null) {
-                session.close();
-            }
-        }
-    }
-
-    private static void getProductByIdCostumer(int id) {
-
-        try (SessionFactory factory = new Configuration()
-                .configure("hibernate.cfg.xml")
-                .buildSessionFactory();
-             Session session = factory.getCurrentSession()) {
-            session.beginTransaction();
-            Costumer costumer = session.get(Costumer.class, id);
-            for (Product p: costumer.getProducts()) {
-                System.out.println(p.getTitle());
-            }
-            System.out.println(costumer);
-                    session.getTransaction().commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
 }
--- spring-boot-hw4/src/main/java/com/example/springboothw4/lesson6/createDB.sql
+++ spring-boot-hw4/src/main/java/com/example/springboothw4/lesson6/createDB.sql
@@ -3,9 +3,11 @@ BEGIN;
 DROP TABLE IF EXISTS costumers_products CASCADE;
 
 DROP TABLE IF EXISTS costumers CASCADE;
-CREATE TABLE costumers (
-    id serial PRIMARY KEY,
-    name VARCHAR(255));
+CREATE TABLE costumers
+(
+    id   serial PRIMARY KEY,
+    name VARCHAR(255)
+);
 INSERT INTO costumers (name) VALUES ('Mike'), ('Jone'), ('Mary');
 
 
@@ -21,5 +23,13 @@ VALUES ('Ipad', 1250.00),
        ('Iphone', 1050.00),
        ('AirPods', 890.00),
        ('MacBookPro', 2500);
+
+
+DROP TABLE IF EXISTS costumers_products CASCADE;
+CREATE TABLE costumers_products (
+                                    costumer_id int REFERENCES costumers (id) ON UPDATE CASCADE ON DELETE CASCADE,
+                                    product_id int REFERENCES products (id) ON UPDATE CASCADE,
+                                    CONSTRAINT costumers_products_pkey PRIMARY KEY (costumer_id, product_id)
+);
 
 COMMIT;
